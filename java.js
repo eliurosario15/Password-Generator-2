a1923337e69d10780fb7bed43a03dc669da1ea73
Eliu Rosario <eliurosario@gmail.com>
2022-10-20 20:07:16 -0400

    Created 4 arrays with the 4 type of character options

--- java.js
+++ java.js
@@ -1,5 +1,25 @@
 // Assignment code here
 
+//These are my arrays//
+
+let lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let symbols = ["!", "@", "#", "$", "%", "&", "*", "=", "+"];
+let upperCase = lowerCase.map(function(x){
+    return x.toLocaleUpperCase();
+})
+
+
+
+//Windows prompt//
+
+function generatePassword () {
+
+    let passwordInput = window.prompt("How long do you want your password to be?")
+    
+
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
