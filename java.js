110b4946af2464f9e48c55207627fc3656ff8c5f
Eliu Rosario <eliurosario@gmail.com>
2022-10-20 21:13:49 -0400

    Modified the upperCase array

--- java.js
+++ java.js
@@ -5,9 +5,7 @@
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var symbols = ["!", "@", "#", "$", "%", "&", "*", "=", "+"];
-var upperCase = lowerCase.map(function(x){
-    return x.toUpperCase();
-})
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
 let userSelection = [];
 
@@ -37,8 +35,13 @@ if (confirm("Do you want the password to include symbols?"))
 userSelection.push(...symbols);
 
 if (userSelection == 0) {
-    window.alert("You must select a crietria")
+    window.alert("You must select a criteria")
+    return
 }
+
+console.log(userSelection);
+
+
 
 
 
