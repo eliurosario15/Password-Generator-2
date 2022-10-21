51e7b735cd346ef0d1a419c78fc18f8405166afa
Eliu Rosario <eliurosario@gmail.com>
2022-10-20 20:33:34 -0400

    Created function that selects arrays based on the user choice

--- java.js
+++ java.js
@@ -1,25 +1,41 @@
 // Assignment code here
 
-//These are my arrays//
+//These are the arrays with the character options//
 
-let lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-let symbols = ["!", "@", "#", "$", "%", "&", "*", "=", "+"];
-let upperCase = lowerCase.map(function(x){
-    return x.toLocaleUpperCase();
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var symbols = ["!", "@", "#", "$", "%", "&", "*", "=", "+"];
+var upperCase = lowerCase.map(function(x){
+    return x.toUpperCase();
 })
 
+let userSelection = [];
+
+if (confirm("Do you want the password to include lower case letters?"))
+userSelection.push(...lowerCase);
+if (confirm("Do you want the password to include upper case letters?"))
+userSelection.push(...upperCase);
+if (confirm("Do you want the password to include numbers?"))
+userSelection.push(...numbers);
+if (confirm("Do you want the password to include symbols?"))
+userSelection.push(...symbols);
+
+console.log(userSelection);
 
 
-//Windows prompt//
 
-function generatePassword () {
 
-    let passwordInput = window.prompt("How long do you want your password to be?")
+
+
+
+
+
+
+  
     
 
 
-}
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
